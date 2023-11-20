2a288a0a8293a1a767d203f2490bccde6446c398
achrafabbes99 <achraf_ab1999@hotmail.fr>
2023-11-20 13:33:54 +0100

    first commit

---
+++ src/app/components/home-one/home-one.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'owl.carousel';
+
+@Component({
+  selector: 'app-home-one',
+  templateUrl: './home-one.component.html',
+  styleUrls: ['./home-one.component.scss']
+})
+export class HomeOneComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
